a64ea63c0aa568ed064a37360b3fd06c8c98b673
gurjotsaini <gurjotsaini1992@gmail.com>
2019-04-21 00:29:53 +0530

    Added: posts access control method

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -5,6 +5,14 @@
 
     class Posts extends Controller
     {
+        public function __construct () {
+            // Check to see if $_SESSION['user_id'] is there
+            // Or, check to see if user is logged in or not
+            if (!isLoggedIn()) {
+                redirect('users/login');
+            }
+        }
+
         public function index() {
             $data = [
                 'page_title'    =>  'Posts'
